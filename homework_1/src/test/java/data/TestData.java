a99241766b119c8be5bf47f07df10babe0b3f1a1
itsskey <109790338+itsskey@users.noreply.github.com>
2025-03-27 01:19:14 +0300

    Delete homework_1 directory

--- homework_1/src/test/java/data/TestData.java
+++
@@ -1,9 +0,0 @@
-package data;
-
-public class TestData {
-    public static final String VALID_LOGIN = "79292086450";
-    public static final String VALID_PASSWORD = "ytrewq1234";
-    public static final String INVALID_LOGIN = "invalid_login";
-    public static final String INVALID_PASSWORD = "invalid_password";
-    public static final String USER_BIRTH_DATE = "11 января 2004 (21 год)";
-}
